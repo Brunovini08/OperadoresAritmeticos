ecceb0525a76d4a9e9e5bb95d1cd67a2e0c54065
Bruno Vinícius <brunovinicius082005@gmail.com>
2023-03-18 13:37:22 -0300

    Operador Aritmetico

---
+++ script.js
@@ -0,0 +1,46 @@
+//OPERADORES ARITMÉTICOS
+// + ADICAO
+// - SUBTRACAO
+// * MULTIPLICACAO
+// / DIVISAO
+// % RESTO DA DIVISAO
+// ** POTENCIACAO
+
+const num1 = 5;
+const num2 = 10;
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+console.log(num1 ** num2);
+
+//INCREMENTO
+//++
+
+/*Existe dois tipos de incremento, o pre incremento e o pos incremento,
+o pre incremeto pega o valor e adiciona mais um,
+o pos incremento pega o valor, mostra ele e depois acrescenta mais um
+*/
+
+let contador = 1;
+contador++
+console.log(contador);
+
+//DECREMENTO
+
+/*
+Existe dois tipos de decremento, o pre decremento e o pos decremento,
+o pre decremeto pega o valor e subtrai um,
+o pos decremento pega o valor, mostra ele e depois subtrai um
+*/
+
+contador--;
+console.log(contador);
+
+ // NsN = Not s numbber
+ const numero1 = 10;
+ const numero2 = parseFloat('5.2');
+ const numero3 = Number('5');
+ console.log(numero1 + numero2);
+ console.log(typeof(numero2));
